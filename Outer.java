68502917c94527edbea06b12a063341a9a4e9f4a
Aaron Hornby <35550004+ajhornby@users.noreply.github.com>
2018-03-25 14:43:19 -0600

    a test file

---
+++ Outer.java
@@ -0,0 +1,36 @@
+package main;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.io.*; 
+
+public class Outer {
+	
+	int x;
+	
+	public int m() {
+		
+		class Local {
+			Local[] l2; 
+		}	
+		
+		Local l3; 
+		Local[] l; 
+		
+		return 0; 
+	}
+	
+	public void m2(int i) {
+		
+	}
+	
+	int[] xs = new int[2]; 
+	
+	Map<String[], int[]> map = new HashMap<String[], int[]>(); 
+	
+	Map<String, Integer[]>[] mapArr; 
+	
+	int[][] xss; 
+	
+	
+}
